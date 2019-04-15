fcd346dd2b21a3b4ecc0f635b39109b10a8ba969
Rizky Alfa Uji G <rizkyalfauji11@gmail.com>
2019-04-15 13:55:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Azure Submission 1</title>
+</head>
+<body>
+	<h1>Register Here</h1>
+	<label>Fill in your name and email address, then click Submit to register.</label>
+	<form>
+	<table method="post" action="index.php" enctype="multipart/form-data">
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td>Job</td>
+			<td><input type="text" name="job"></td>
+		</tr>
+		<tr>
+			<td><button name="submit">Submit</button></td>
+			<td><button name="load">Load Data</button></td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
+<?php  
+
+	$host = "dicodingwebapps.database.windows.net";
+	$user = "dicoding";
+	$pass = "Dadangkole0";
+	$db = "dicodingwebapp";
+
+	try{
+		$connect = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+		$connect->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(Exception $e){
+		echo "Failed: ".$e;
+	}
+?>
